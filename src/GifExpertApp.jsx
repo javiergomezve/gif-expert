bc0e69ea4a9847d9b17cf75e250effa1dda2f8d3
Javier Gomez <javiergomezve@gmail.com>
2020-10-10 13:52:39 -0500

    [add] Categories: list and form

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {AddCategory} from "./components/AddCategory";
 
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
 
     return (
         <div>
             <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories} />
             <hr/>
+
+
+            <ol>
+                {categories.map(category => (
+                    <li key={category}>{category}</li>
+                ))}
+            </ol>
         </div>
     );
 }
